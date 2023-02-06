1cfec67b222e632cf4e99946a68b5483ac9af66c
BinxuWang <binxu.wang@wustl.edu>
2023-02-06 00:36:06 -0600

    add

--- core/utils/CNN_scorers.py
+++ core/utils/CNN_scorers.py
@@ -28,7 +28,7 @@ if platform == "linux": # cluster
         torchhome = join(scratchdir, "torch/checkpoints")  # RIS
     else:
         # torchhome = torch.hub._get_torch_home()
-        torchhome = torch.hub.get_dir()  # torch.hub._get_torch_home()
+        torchhome = join(torch.hub.get_dir(), "checkpoints")  # torch.hub._get_torch_home()
 else:
     if os.environ['COMPUTERNAME'] == 'DESKTOP-9DDE2RH':  # PonceLab-Desktop 3
         torchhome = r"E:\Cluster_Backup\torch"
