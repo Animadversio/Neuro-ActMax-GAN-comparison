63b7954e5aa781ce5a98b13d4a326ebb4dd3ec9c
BinxuWang <binxu.wang@wustl.edu>
2023-02-09 17:06:09 -0600

    add Evolution combined analysis

--- insilico_experiments/BigGAN_gradEvol_combined_analysis.py
+++ insilico_experiments/BigGAN_gradEvol_combined_analysis.py
@@ -18,14 +18,7 @@ from collections import defaultdict
 import pickle as pkl
 from core.utils.plot_utils import saveallforms
 sumdir = r"F:\insilico_exps\GAN_Evol_cmp\summary"
-#%%
-unitdirname = r"resnet50_.layer3.Bottleneck5_9_7_7_RFrsz"
-grad_scores_fn = Path(figdir) / f"{unitdirname}_proto_scores.pkl"
-evol_scores_fn = Path(figdir) / f"{unitdirname}_evolproto_info.pkl"
-grad_scores = pkl.load(open(grad_scores_fn, "rb"))
-evol_scores = pkl.load(open(evol_scores_fn, "rb"))
-#%%
-pd.DataFrame(evol_scores["CholCMA"])
+
 #%%
 grad_df = pd.read_csv(Path(sumdir) / f"raw_summary_grad.csv", index_col=0)
 evol_df = pd.read_csv(Path(sumdir) / f"raw_summary.csv", )
@@ -35,7 +28,10 @@ evol_df_reslinf8 = pd.read_csv(Path(r"E:\Cluster_Backup\GAN_Evol_cmp\summary") /
 evol_df_reslinf8["netname"] = evol_df_reslinf8["netname"].str.replace("resnet50_linf_8", "resnet50_linf8")
 #%%
 cmb_df  = pd.concat((evol_df, evol_df_reslinf8, grad_df), axis=0)  # pd.merge(grad_df, evol_df, on=["netname", "layer", "unitid", "x", "y", "RFresize"])
+cmb_df.to_csv(Path(sumdir) / f"raw_summary_cmb_grad-evol.csv")
 #%%
+"""Average scores for each unit x method x GAN
+Then pivot the table to get a matrix of scores for each unit x method x GAN"""
 meansummary = cmb_df.groupby(by=["netname", "layer", "unitid", "RFresize", "optimmethod", "GANname"], dropna=False).mean()
 mean_pivot = meansummary.pivot_table(index=["netname", "layer", "unitid", "RFresize"], columns=["GANname", "optimmethod", ], values="score")
 #%%
@@ -44,13 +40,14 @@ meansummary.to_csv(Path(sumdir) / f"optimizer_unit_summary_raw.csv")
 
 
 #%%
+"""Find the units with paired gradient & gradeint free evolutions """
 targetoptims = ["Adam001", "Adam001Hess", "CholCMA", "HessCMA",
                 "Adam01",  "Adam01Hess", "HessCMA500_fc6", ]
 targetoptims = [("BigGAN", "Adam001"), ("BigGAN", "Adam001Hess"), ("BigGAN", "CholCMA"), ("BigGAN", "HessCMA"),
                 ("fc6", "Adam01"),  ("fc6", "Adam01Hess"), ("fc6", "HessCMA500_fc6"), ]
 # find rows in which all targetoptims are has non nan value in mean_pivot
 targetrows = mean_pivot.loc[:, targetoptims].notna().all(axis=1)
-#%%
+# filter the table with these matched methods
 mean_pivot_matched = mean_pivot[targetrows].loc[:, targetoptims]
 #%%
 mean_pivot_matched.to_csv(Path(sumdir) / f"optimizer_unit_summary_matched.csv")
@@ -60,6 +57,7 @@ plt.savefig(Path(sumdir) / f"optimizer_unit_summary_matched.png")
 plt.show()
 
 #%%
+"""Normalize each unit's score by the max score across methods of the unit"""
 layermean_pivot_matched = mean_pivot_matched\
     .groupby(by=["netname", "layer", "RFresize"]).mean()
 layermean_pivot_matched.T.plot(kind="bar", figsize=(6, 4))
@@ -93,4 +91,11 @@ for netname in ["resnet50_linf8", "resnet50"]:
 
 
 
-
+#%%
+# unitdirname = r"resnet50_.layer3.Bottleneck5_9_7_7_RFrsz"
+# grad_scores_fn = Path(figdir) / f"{unitdirname}_proto_scores.pkl"
+# evol_scores_fn = Path(figdir) / f"{unitdirname}_evolproto_info.pkl"
+# grad_scores = pkl.load(open(grad_scores_fn, "rb"))
+# evol_scores = pkl.load(open(evol_scores_fn, "rb"))
+# #%%
+# pd.DataFrame(evol_scores["CholCMA"])
