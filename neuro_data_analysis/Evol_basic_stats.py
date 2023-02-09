b54e3ab73670ab231ca8ce25b5b18ca73154d016
BinxuWang <binxu.wang@wustl.edu>
2023-02-09 13:45:42 -0600

    plotting code for in vivo data

--- neuro_data_analysis/Evol_basic_stats.py
+++ neuro_data_analysis/Evol_basic_stats.py
@@ -17,6 +17,12 @@ BFEStats_merge, BFEStats = load_neural_data()
 #%%
 sumdir = r"E:\OneDrive - Harvard University\Manuscript_BigGAN\Figures\Evol_activation_stats"
 acttab = pd.read_csv(join(sumdir, "act_increase_summary.csv"))
+#%% masks for plotting errorbar
+Alfamsk = acttab.Animal =="Alfa"
+Betomsk = acttab.Animal =="Beto"
+V1msk = acttab.area =="V1"
+V4msk = acttab.area =="V4"
+ITmsk = acttab.area =="IT"
 #%%
 # BGerr = np.stack((acttab.BigGAN_endinit_CI_1, acttab.BigGAN_endinit_CI_2),axis=1) \
 #        - acttab.BigGAN_endinit_m.to_numpy()[:,None]
@@ -52,11 +58,6 @@ plt.ylabel("fc6 end - init (events/sec)")
 plt.axis("equal")
 saveallforms(sumdir, "BGfc6_endinit_all_unit")
 plt.show()
-#%%
-Alfamsk = acttab.Animal =="Alfa"
-Betomsk = acttab.Animal =="Beto"
-V4msk = acttab.area =="V4"
-ITmsk = acttab.area =="IT"
 #%%
 plt.figure(figsize=(6, 6))
 for label, msk in zip(["V4", "IT"], [V4msk, ITmsk]):
@@ -126,11 +127,11 @@ plt.ylabel("fc6 init (events/sec)")
 plt.title("Comparison of initial activation in BigGAN and fc6 evolution")
 saveallforms(sumdir, "BGfc6_init_anim_sep")
 plt.show()
+
 #%%
-unittable = acttab.groupby(by=["Animal", "prefchan"]).mean()
-#%%
-acttab.groupby(by=["Animal", "prefchan", "area"])["ephysFN"].count()
-#%%
+#%% Plot the consistency of the activation increase across repetitions
+# acttab.groupby(by=["Animal", "prefchan", "area"])["ephysFN"].count()
+# get default color order
 colororder = plt.rcParams['axes.prop_cycle'].by_key()['color']
 unit_filter = acttab.groupby(by=["Animal", "prefchan", "area"])["ephysFN"].count() > 1
 rep_units = unit_filter.index[unit_filter]
@@ -144,8 +145,8 @@ for ui, unit in enumerate(rep_units):
     ax = axs.flatten()[ui]
     # ax.plot(parttab.BigGAN_endinit_m, parttab.fc6_endinit_m, "o", )
     ax.errorbar(parttab.BigGAN_endinit_m, parttab.fc6_endinit_m,
-                parttab.BigGAN_init_CI_2 - parttab.BigGAN_init_m,
-                parttab.fc6_init_CI_2 - parttab.fc6_init_m, fmt="o", color=clr)
+                parttab.BigGAN_endinit_CI_2 - parttab.BigGAN_endinit_m,
+                parttab.fc6_endinit_CI_2 - parttab.fc6_endinit_m, fmt="o", color=clr)
     ax.axline((0, 0), (1, 1), color="k")
     ax.axvline(0, color="k", alpha=0.5)
     ax.axhline(0, color="k", alpha=0.5)
@@ -157,6 +158,117 @@ figh.suptitle("Repetition consistency of activation increase in BigGAN and fc6 e
 plt.tight_layout()
 saveallforms(sumdir, "BGfc6_endinit_unit_rep_consistency")
 plt.show()
-#%%
+#%% Key utils function
+def errorbar_per_mask(x, y, xerr, yerr, msks, labels, ax=None, **kwargs):
+    if ax is None:
+        ax = plt.gca()
+    if msks is None or len(msks) == 0:
+        msks = [np.ones_like(x, dtype=bool)]
+        labels = ["all"]
+    for msk, label in zip(msks, labels):
+        ax.errorbar(x[msk], y[msk], xerr=xerr[msk], yerr=yerr[msk], label=label,
+                    **kwargs)
+    ax.axline((0, 0), (1, 1), color="k")
+    ax.axvline(0, color="k", alpha=0.5)
+    ax.axhline(0, color="k", alpha=0.5)
+    plt.axis("equal")
+    plt.legend()
 
-# get default color order
+
+#%%
+plt.figure(figsize=(6, 6))
+errorbar_per_mask(acttab.BigGAN_endinit_m, acttab.fc6_endinit_m,
+          xerr=acttab.BigGAN_endinit_CI_2 - acttab.BigGAN_endinit_m,
+          yerr=acttab.fc6_endinit_CI_2 - acttab.fc6_endinit_m,
+      msks=[Alfamsk, Betomsk], labels=["Alfa", "Beto"], fmt=".", alpha=0.45)
+plt.xlabel("BigGAN end - init (events/sec)")
+plt.ylabel("fc6 end - init (events/sec)")
+plt.title("Comparison of activation increase in BigGAN and fc6 evolution")
+saveallforms(sumdir, "BGfc6_endinit_anim_sep")
+plt.show()
+#%%
+plt.figure(figsize=(6, 6))
+errorbar_per_mask(acttab.BigGAN_init_m, acttab.fc6_init_m,
+          xerr=acttab.BigGAN_init_CI_2 - acttab.BigGAN_init_m,
+          yerr=acttab.fc6_init_CI_2 - acttab.fc6_init_m,
+      msks=[Alfamsk, Betomsk], labels=["Alfa", "Beto"], fmt=".", alpha=0.45)
+plt.xlabel("BigGAN init (events/sec)")
+plt.ylabel("fc6 init (events/sec)")
+plt.title("Comparison of initial activation in BigGAN and fc6 evolution")
+saveallforms(sumdir, "BGfc6_initact_anim_sep")
+plt.show()
+#%%
+plt.figure(figsize=(6, 6))
+errorbar_per_mask(acttab.BigGAN_end_m, acttab.fc6_end_m,
+          xerr=acttab.BigGAN_end_CI_2 - acttab.BigGAN_end_m,
+          yerr=acttab.fc6_end_CI_2 - acttab.fc6_end_m,
+      msks=[Alfamsk, Betomsk], labels=["Alfa", "Beto"], fmt=".", alpha=0.45)
+plt.xlabel("BigGAN end activation (events/sec)")
+plt.ylabel("fc6 end activation (events/sec)")
+plt.title("Comparison of final activation in BigGAN and fc6 evolution")
+saveallforms(sumdir, "BGfc6_endact_anim_sep")
+plt.show()
+#%%
+plt.figure(figsize=(6, 6))
+errorbar_per_mask(acttab.BigGAN_endinit_m, acttab.fc6_endinit_m,
+          xerr=acttab.BigGAN_endinit_CI_2 - acttab.BigGAN_endinit_m,
+          yerr=acttab.fc6_endinit_CI_2 - acttab.fc6_endinit_m,
+      msks=[V4msk, ITmsk], labels=["V4", "IT"], fmt=".", alpha=0.3)
+plt.xlabel("BigGAN end - init (events/sec)")
+plt.ylabel("fc6 end - init (events/sec)")
+plt.title("Comparison of activation increase in BigGAN and fc6 evolution")
+saveallforms(sumdir, "BGfc6_endinit_area_sep")
+plt.show()
+#%%
+for msk, area in zip([V1msk, V4msk, ITmsk], ["V1", "V4", "IT"]):
+    plt.figure(figsize=(6, 6))
+    errorbar_per_mask(acttab.BigGAN_endinit_m, acttab.fc6_endinit_m,
+              xerr=acttab.BigGAN_endinit_CI_2 - acttab.BigGAN_endinit_m,
+              yerr=acttab.fc6_endinit_CI_2 - acttab.fc6_endinit_m,
+          msks=[msk, ], labels=[area, ], fmt=".", alpha=0.3)
+    plt.xlabel("BigGAN end - init (events/sec)")
+    plt.ylabel("fc6 end - init (events/sec)")
+    plt.title("Comparison of activation increase in BigGAN and fc6 evolution")
+    saveallforms(sumdir, f"BGfc6_endinit_{area}only")
+    plt.show()
+#%% activation increase through Evolution
+for msk, area in zip([V1msk, V4msk, ITmsk], ["V1", "V4", "IT"]):
+    plt.figure(figsize=(6, 6))
+    errorbar_per_mask(acttab.BigGAN_endinit_m, acttab.fc6_endinit_m,
+              xerr=acttab.BigGAN_endinit_CI_2 - acttab.BigGAN_endinit_m,
+              yerr=acttab.fc6_endinit_CI_2 - acttab.fc6_endinit_m,
+          msks=[msk & Alfamsk, msk & Betomsk], labels=["Alfa-" + area, "Beto-" + area],
+              fmt=".", alpha=0.45)
+    plt.xlabel("BigGAN end - init (events/sec)")
+    plt.ylabel("fc6 end - init (events/sec)")
+    plt.title("Comparison of activation increase in BigGAN and fc6 evolution")
+    saveallforms(sumdir, f"BGfc6_endinit_{area}only_anim_sep")
+    plt.show()
+
+#%% end generation activation
+for msk, area in zip([V1msk, V4msk, ITmsk], ["V1", "V4", "IT"]):
+    plt.figure(figsize=(6, 6))
+    errorbar_per_mask(acttab.BigGAN_end_m, acttab.fc6_end_m,
+              xerr=acttab.BigGAN_end_CI_2 - acttab.BigGAN_end_m,
+              yerr=acttab.fc6_end_CI_2 - acttab.fc6_end_m,
+            msks=[msk & Alfamsk, msk & Betomsk], labels=["Alfa-" + area, "Beto-" + area],
+                      fmt=".", alpha=0.45)
+    plt.xlabel("BigGAN end activation (events/sec)")
+    plt.ylabel("fc6 end activation (events/sec)")
+    plt.title("Comparison of end activation in BigGAN and fc6 evolution")
+    saveallforms(sumdir, f"BGfc6_endact_{area}only_anim_sep")
+    plt.show()
+
+#%% initial generation activation
+for msk, area in zip([V1msk, V4msk, ITmsk], ["V1", "V4", "IT"]):
+    plt.figure(figsize=(6, 6))
+    errorbar_per_mask(acttab.BigGAN_init_m, acttab.fc6_init_m,
+              xerr=acttab.BigGAN_init_CI_2 - acttab.BigGAN_init_m,
+              yerr=acttab.fc6_init_CI_2 - acttab.fc6_init_m,
+            msks=[msk & Alfamsk, msk & Betomsk], labels=["Alfa-" + area, "Beto-" + area],
+                      fmt=".", alpha=0.45)
+    plt.xlabel("BigGAN initial activation (events/sec)")
+    plt.ylabel("fc6 initial activation (events/sec)")
+    plt.title("Comparison of initial activation in BigGAN and fc6 evolution")
+    saveallforms(sumdir, f"BGfc6_initact_{area}only_anim_sep")
+    plt.show()
