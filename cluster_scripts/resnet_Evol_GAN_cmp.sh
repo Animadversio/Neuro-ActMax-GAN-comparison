433f98cb3b65fea801dc05f37dde2f91c96bd848
BinxuWang <binxu.wang@wustl.edu>
2023-02-10 00:29:00 -0600

    add resnet tasks

--- cluster_scripts/resnet_Evol_GAN_cmp.sh
+++ cluster_scripts/resnet_Evol_GAN_cmp.sh
@@ -4,7 +4,7 @@
 #SBATCH -t 8:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16G
-#SBATCH --array=1-2
+#SBATCH --array=43-46
 #SBATCH --mail-user=binxu_wang@hms.harvard.edu
 #SBATCH -o BigGANresnet_evol_%j.out
 
@@ -53,6 +53,10 @@ param_list=\
 --chans 25 50 --G fc6    --net resnet50_linf8 --layer .layer3.Bottleneck5 --optim HessCMA500 CholCMA  --rep 10
 --chans 0  25 --G BigGAN --net resnet50_linf8 --layer .layer3.Bottleneck5 --optim HessCMA CholCMA --rep 10
 --chans 25 50 --G BigGAN --net resnet50_linf8 --layer .layer3.Bottleneck5 --optim HessCMA CholCMA --rep 10
+--chans 0  25 --G fc6    --net resnet50_linf8 --layer .layer3.Bottleneck5 --optim HessCMA500 CholCMA  --rep 10 --RFresize 1
+--chans 25 50 --G fc6    --net resnet50_linf8 --layer .layer3.Bottleneck5 --optim HessCMA500 CholCMA  --rep 10 --RFresize 1
+--chans 0  25 --G BigGAN --net resnet50_linf8 --layer .layer3.Bottleneck5 --optim HessCMA CholCMA --rep 10 --RFresize 1
+--chans 25 50 --G BigGAN --net resnet50_linf8 --layer .layer3.Bottleneck5 --optim HessCMA CholCMA --rep 10 --RFresize 1
 '
 
 export unit_name="$(echo "$param_list" | head -n $SLURM_ARRAY_TASK_ID | tail -1)"
