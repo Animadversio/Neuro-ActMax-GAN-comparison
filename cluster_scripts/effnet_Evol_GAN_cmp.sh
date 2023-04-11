f16f7c9e7ebb89346b6b80864f9feefae0ed0df2
BinxuWang <binxu.wang@wustl.edu>
2023-04-11 12:04:49 -0500

    add final tasks

--- cluster_scripts/effnet_Evol_GAN_cmp.sh
+++ cluster_scripts/effnet_Evol_GAN_cmp.sh
@@ -4,7 +4,7 @@
 #SBATCH -t 8:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16G
-#SBATCH --array=37-72
+#SBATCH --array=73-108
 #SBATCH --mail-user=binxu_wang@hms.harvard.edu
 #SBATCH -o BigGANeffnet_evol_%j.out
 
@@ -83,6 +83,42 @@ param_list=\
 --chans 10 50 --net tf_efficientnet_b6 --layer .SelectAdaptivePool2dglobal_pool --optim HessCMA CholCMA --G BigGAN --rep 10
 --chans 10 50 --net tf_efficientnet_b6 --layer .Linearclassifier --optim HessCMA500 CholCMA --G fc6 --rep 10
 --chans 10 50 --net tf_efficientnet_b6 --layer .Linearclassifier --optim HessCMA CholCMA --G BigGAN --rep 10
+--chans 40 50 --net tf_efficientnet_b6_ap --layer .blocks.0 --optim HessCMA500 CholCMA --G fc6 --rep 10
+--chans 40 50 --net tf_efficientnet_b6_ap --layer .blocks.0 --optim HessCMA CholCMA --G BigGAN --rep 10
+--chans 40 50 --net tf_efficientnet_b6_ap --layer .blocks.1 --optim HessCMA500 CholCMA --G fc6 --rep 10
+--chans 40 50 --net tf_efficientnet_b6_ap --layer .blocks.1 --optim HessCMA CholCMA --G BigGAN --rep 10
+--chans 40 50 --net tf_efficientnet_b6_ap --layer .blocks.2 --optim HessCMA500 CholCMA --G fc6 --rep 10
+--chans 40 50 --net tf_efficientnet_b6_ap --layer .blocks.2 --optim HessCMA CholCMA --G BigGAN --rep 10
+--chans 40 50 --net tf_efficientnet_b6_ap --layer .blocks.3 --optim HessCMA500 CholCMA --G fc6 --rep 10
+--chans 40 50 --net tf_efficientnet_b6_ap --layer .blocks.3 --optim HessCMA CholCMA --G BigGAN --rep 10
+--chans 40 50 --net tf_efficientnet_b6_ap --layer .blocks.4 --optim HessCMA500 CholCMA --G fc6 --rep 10
+--chans 40 50 --net tf_efficientnet_b6_ap --layer .blocks.4 --optim HessCMA CholCMA --G BigGAN --rep 10
+--chans 40 50 --net tf_efficientnet_b6_ap --layer .blocks.5 --optim HessCMA500 CholCMA --G fc6 --rep 10
+--chans 40 50 --net tf_efficientnet_b6_ap --layer .blocks.5 --optim HessCMA CholCMA --G BigGAN --rep 10
+--chans 40 50 --net tf_efficientnet_b6_ap --layer .blocks.6 --optim HessCMA500 CholCMA --G fc6 --rep 10
+--chans 40 50 --net tf_efficientnet_b6_ap --layer .blocks.6 --optim HessCMA CholCMA --G BigGAN --rep 10
+--chans 40 50 --net tf_efficientnet_b6 --layer .blocks.0 --optim HessCMA500 CholCMA --G fc6 --rep 10
+--chans 40 50 --net tf_efficientnet_b6 --layer .blocks.0 --optim HessCMA CholCMA --G BigGAN --rep 10
+--chans 40 50 --net tf_efficientnet_b6 --layer .blocks.1 --optim HessCMA500 CholCMA --G fc6 --rep 10
+--chans 40 50 --net tf_efficientnet_b6 --layer .blocks.1 --optim HessCMA CholCMA --G BigGAN --rep 10
+--chans 40 50 --net tf_efficientnet_b6 --layer .blocks.2 --optim HessCMA500 CholCMA --G fc6 --rep 10
+--chans 40 50 --net tf_efficientnet_b6 --layer .blocks.2 --optim HessCMA CholCMA --G BigGAN --rep 10
+--chans 40 50 --net tf_efficientnet_b6 --layer .blocks.3 --optim HessCMA500 CholCMA --G fc6 --rep 10
+--chans 40 50 --net tf_efficientnet_b6 --layer .blocks.3 --optim HessCMA CholCMA --G BigGAN --rep 10
+--chans 40 50 --net tf_efficientnet_b6 --layer .blocks.4 --optim HessCMA500 CholCMA --G fc6 --rep 10
+--chans 40 50 --net tf_efficientnet_b6 --layer .blocks.4 --optim HessCMA CholCMA --G BigGAN --rep 10
+--chans 40 50 --net tf_efficientnet_b6 --layer .blocks.5 --optim HessCMA500 CholCMA --G fc6 --rep 10
+--chans 40 50 --net tf_efficientnet_b6 --layer .blocks.5 --optim HessCMA CholCMA --G BigGAN --rep 10
+--chans 40 50 --net tf_efficientnet_b6 --layer .blocks.6 --optim HessCMA500 CholCMA --G fc6 --rep 10
+--chans 40 50 --net tf_efficientnet_b6 --layer .blocks.6 --optim HessCMA CholCMA --G BigGAN --rep 10
+--chans 40 50 --net tf_efficientnet_b6_ap --layer .SelectAdaptivePool2dglobal_pool --optim HessCMA500 CholCMA --G fc6 --rep 10
+--chans 40 50 --net tf_efficientnet_b6_ap --layer .SelectAdaptivePool2dglobal_pool --optim HessCMA CholCMA --G BigGAN --rep 10
+--chans 40 50 --net tf_efficientnet_b6_ap --layer .Linearclassifier --optim HessCMA500 CholCMA --G fc6 --rep 10
+--chans 40 50 --net tf_efficientnet_b6_ap --layer .Linearclassifier --optim HessCMA CholCMA --G BigGAN --rep 10
+--chans 40 50 --net tf_efficientnet_b6 --layer .SelectAdaptivePool2dglobal_pool --optim HessCMA500 CholCMA --G fc6 --rep 10
+--chans 40 50 --net tf_efficientnet_b6 --layer .SelectAdaptivePool2dglobal_pool --optim HessCMA CholCMA --G BigGAN --rep 10
+--chans 40 50 --net tf_efficientnet_b6 --layer .Linearclassifier --optim HessCMA500 CholCMA --G fc6 --rep 10
+--chans 40 50 --net tf_efficientnet_b6 --layer .Linearclassifier --optim HessCMA CholCMA --G BigGAN --rep 10
 '
 
 export unit_name="$(echo "$param_list" | head -n $SLURM_ARRAY_TASK_ID | tail -1)"
