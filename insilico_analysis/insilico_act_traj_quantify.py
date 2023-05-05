8f1d199d7960c4071b9bcdcdd5db63c17ab7665c
BinxuWang <binxu.wang@wustl.edu>
2023-05-05 12:28:00 -0500

    adding activation trajectory across all layers visualize

--- insilico_analysis/insilico_act_traj_quantify.py
+++ insilico_analysis/insilico_act_traj_quantify.py
@@ -47,8 +47,10 @@ def _procrustes_array(arr, desired_len=10, axis=0):
 optimnames = "CholCMA", "HessCMA", "HessCMA500_fc6"
 score_mean1_col = []
 score_mean2_col = []
+score_mean3_col = []
 score_sem1_col = []
 score_sem2_col = []
+score_sem3_col = []
 FC_win_prob_col = []
 BG_win_prob_col = []
 FC_BG_tval_col = []
@@ -56,9 +58,11 @@ for unitname in tqdm(unitlist):
     # unitdir = join(r"F:\insilico_exps\GAN_Evol_cmp", unitname)
     score_traj_dict = pkl.load(open(join(outdir, f"{unitname}_score_traj_dict.pkl"), "rb"))
     score_mean1 = score_traj_dict['HessCMA500_fc6']['scores_mean']
-    score_mean2 = score_traj_dict['HessCMA']['scores_mean']
+    score_mean2 = score_traj_dict['CholCMA']['scores_mean']
+    score_mean3 = score_traj_dict['HessCMA']['scores_mean']
     score_sem1 = score_traj_dict['HessCMA500_fc6']['scores_sem']
-    score_sem2 = score_traj_dict['HessCMA']['scores_sem']
+    score_sem2 = score_traj_dict['CholCMA']['scores_sem']
+    score_sem3 = score_traj_dict['HessCMA']['scores_sem']
     # cross compare
     FC_win = score_mean1[:, None] - score_mean2[None, :] > 2 * np.sqrt(score_sem1[:, None]**2 + score_sem2[None, :]**2)
     BG_win = score_mean2[None, :] - score_mean1[:, None] > 2 * np.sqrt(score_sem1[:, None]**2 + score_sem2[None, :]**2)
@@ -68,22 +72,27 @@ for unitname in tqdm(unitlist):
     FC_BG_tval_mean = np.mean(FC_BG_tval, axis=(0, 1))
     score_mean1_col.append(_procrustes_array(score_mean1))
     score_mean2_col.append(_procrustes_array(score_mean2))
+    score_mean3_col.append(_procrustes_array(score_mean3))
     score_sem1_col.append(_procrustes_array(score_sem1))
     score_sem2_col.append(_procrustes_array(score_sem2))
+    score_sem3_col.append(_procrustes_array(score_sem3))
     FC_win_prob_col.append(FC_win_prob)
     BG_win_prob_col.append(BG_win_prob)
     FC_BG_tval_col.append(FC_BG_tval_mean)
 
 score_mean1_col = np.stack(score_mean1_col, axis=0)
 score_mean2_col = np.stack(score_mean2_col, axis=0)
+score_mean3_col = np.stack(score_mean3_col, axis=0)
 score_sem1_col = np.stack(score_sem1_col, axis=0)
 score_sem2_col = np.stack(score_sem2_col, axis=0)
+score_sem3_col = np.stack(score_sem3_col, axis=0)
 FC_win_prob_col = np.stack(FC_win_prob_col, axis=0)
 BG_win_prob_col = np.stack(BG_win_prob_col, axis=0)
 FC_BG_tval_col = np.stack(FC_BG_tval_col, axis=0)
 #%%
 np.savez(join(figdir, "insilico_act_traj_synopsis.npz"), score_mean1_col=score_mean1_col,
-            score_mean2_col=score_mean2_col, score_sem1_col=score_sem1_col, score_sem2_col=score_sem2_col,
+            score_mean2_col=score_mean2_col, score_mean3_col=score_mean3_col,
+            score_sem1_col=score_sem1_col, score_sem2_col=score_sem2_col, score_sem3_col=score_sem3_col,
             FC_win_prob_col=FC_win_prob_col, BG_win_prob_col=BG_win_prob_col, FC_BG_tval_col=FC_BG_tval_col)
 
 
@@ -139,11 +148,17 @@ def _shaded_errorbar_arr(arr, color="r", alpha=0.3, errtype="sem", **kwargs):
     elif errtype == "std":
         yerr = np.nanstd(arr, axis=0)
     plt.plot(x, y, color=color, **kwargs)
-    plt.fill_between(x, y - yerr, y + yerr, color=color, alpha=alpha)
+    plt.fill_between(x, y - yerr, y + yerr, color=color, alpha=alpha,
+                     label="" if "label" not in kwargs else kwargs["label"]+"_"+errtype)
 #%%
-normalizer = np.nanquantile(score_mean1_col[:, :, -5:], 0.95, axis=(1,2))
+normalizer = np.nanquantile(np.concatenate([score_mean1_col, score_mean2_col, score_mean3_col], axis=1)
+                            [:, :, -5:], 0.95, axis=(1,2))
+# replace 0.0 as np.nan
+normalizer[normalizer == 0.0] = np.nan
+ #np.maximum(normalizer, 1e-5)
 normscore_mean1_col = score_mean1_col / normalizer[:, None, None]
 normscore_mean2_col = score_mean2_col / normalizer[:, None, None]
+normscore_mean3_col = score_mean3_col / normalizer[:, None, None]
 
 #%%
 for (netname, layername, RFrsz), df in meta_info_df.groupby(by=["netname", "layername", "RFrsz"]):
@@ -151,25 +166,30 @@ for (netname, layername, RFrsz), df in meta_info_df.groupby(by=["netname", "laye
     # print(df)
 
     plt.figure()
-    plt.plot(normscore_mean1_col[df.index].reshape(-1,100).T, alpha=0.03, color="b")
-    _shaded_errorbar_arr(normscore_mean1_col[df.index].reshape(-1,100), color="b", alpha=0.3, )
-    plt.plot(normscore_mean2_col[df.index].reshape(-1,100).T, alpha=0.03, color="r")
-    _shaded_errorbar_arr(normscore_mean2_col[df.index].reshape(-1,100), color="r", alpha=0.3, )
+    plt.plot(normscore_mean1_col[df.index].reshape(-1,100).T, alpha=0.03, color="b", label=None)
+    _shaded_errorbar_arr(normscore_mean1_col[df.index].reshape(-1,100), color="b", alpha=0.3, label="DeePSim",)
+    plt.plot(normscore_mean2_col[df.index].reshape(-1,100).T, alpha=0.03, color="r", label=None)
+    _shaded_errorbar_arr(normscore_mean2_col[df.index].reshape(-1,100), color="r", alpha=0.3, label="BG CholCMA")
+    plt.plot(normscore_mean3_col[df.index].reshape(-1,100).T, alpha=0.03, color="g", label=None)
+    _shaded_errorbar_arr(normscore_mean3_col[df.index].reshape(-1,100), color="g", alpha=0.3, label="BG HessCMA")
     plt.title(f"{netname} {layername} {'RF resize' if RFrsz else ''}")
     plt.ylabel("Normalized Activation")
     plt.xlabel("Blocks")
+    plt.legend()#["DeePSim", "BG CholCMA", "BG HessCMA"])
     saveallforms(figdir, f"{netname}_{layername}{'_RFrsz' if RFrsz else ''}_normact_cmp")
     plt.show()
 
     plt.figure()
-    _shaded_errorbar_arr(normscore_mean1_col[df.index].reshape(-1, 100), color="b", alpha=0.3, errtype="std")
-    _shaded_errorbar_arr(normscore_mean2_col[df.index].reshape(-1, 100), color="r", alpha=0.3, errtype="std")
+    _shaded_errorbar_arr(normscore_mean1_col[df.index].reshape(-1, 100), color="b", alpha=0.3, errtype="std", label="DeePSim",)
+    _shaded_errorbar_arr(normscore_mean2_col[df.index].reshape(-1, 100), color="r", alpha=0.3, errtype="std", label="BG CholCMA")
+    _shaded_errorbar_arr(normscore_mean3_col[df.index].reshape(-1, 100), color="g", alpha=0.3, errtype="std", label="BG HessCMA")
     plt.title(f"{netname} {layername} {'RF resize' if RFrsz else ''}")
     plt.ylabel("Normalized Activation")
     plt.xlabel("Blocks")
+    plt.legend()#["DeePSim", "BG CholCMA", "BG HessCMA"])
     saveallforms(figdir, f"{netname}_{layername}{'_RFrsz' if RFrsz else ''}_normact_cmp_std")
     plt.show()
-
+    # raise Exception
     # plt.figure()
     # plt.plot(score_mean1_col[df.index].reshape(-1,100).T, alpha=0.03, color="b")
     # _shaded_errorbar_arr(score_mean1_col[df.index].reshape(-1,100), color="b", alpha=0.3, )
@@ -209,4 +229,71 @@ for (netname, layername, RFrsz), df in meta_info_df.groupby(by=["netname", "laye
     # saveallforms(figdir, f"{netname}_{layername}{'_RFrsz' if RFrsz else ''}_FC_BG_tval")
     # plt.show()
     # raise Exception
+
+#%% Merged plot of all layers for each network
+netname = 'resnet50' # 'resnet50_linf8'
+meta_info_df[meta_info_df.netname==netname].layername.unique()
+layernames = ['.layer1.Bottleneck1', '.layer2.Bottleneck3',
+       '.layer3.Bottleneck5', '.layer4.Bottleneck2', '.Linearfc']
+RFrsz = False
+figh, axs = plt.subplots(1, len(layernames), figsize=(len(layernames)*4, 5.5))
+for li, layername in enumerate(layernames):
+    df = meta_info_df[(meta_info_df.netname==netname) & (meta_info_df.layername==layername)
+                      & (meta_info_df.RFrsz==False)]
+    plt.sca(axs[li])
+    _shaded_errorbar_arr(normscore_mean1_col[df.index].reshape(-1, 100), color="b", alpha=0.3, errtype="std",
+                         label="DeePSim", )
+    _shaded_errorbar_arr(normscore_mean2_col[df.index].reshape(-1, 100), color="r", alpha=0.3, errtype="std",
+                         label="BG CholCMA")
+    _shaded_errorbar_arr(normscore_mean3_col[df.index].reshape(-1, 100), color="g", alpha=0.3, errtype="std",
+                         label="BG HessCMA")
+    plt.title(f"{netname} {layername} {'RF resize' if RFrsz else ''}")
+    plt.ylabel("Normalized Activation")
+    plt.xlabel("Blocks")
+    if li==len(layernames)-1:
+        plt.legend()  # ["DeePSim", "BG CholCMA", "BG HessCMA"])
+plt.suptitle(f"{netname} {'RF resize' if RFrsz else ''} Average Normalized Optimization trajectory")
+plt.tight_layout()
+saveallforms(figdir, f"{netname}_{'_RFrsz' if RFrsz else ''}_merged_normact_cmp_std", figh)
+figh.show()
+for ax in axs:
+    ax.set_xlim([0, 40])
+    # ax.autoscale(enable=True, axis='y', tight=True)
+    ax.autoscale_view(tight=True, scalex=False, scaley=True)
+saveallforms(figdir, f"{netname}_{'_RFrsz' if RFrsz else ''}_merged_normact_cmp_std_xlim40", figh)
+figh.show()
+#%%
+netname = "tf_efficientnet_b6_ap" # 'tf_efficientnet_b6'
+meta_info_df[meta_info_df.netname==netname].layername.unique()
+layernames = ['.blocks.0', '.blocks.1', '.blocks.2', '.blocks.3', '.blocks.4',
+       '.blocks.5', '.blocks.6','.SelectAdaptivePool2dglobal_pool',
+       '.Linearclassifier']
+RFrsz = False
+
+figh, axs = plt.subplots(1, len(layernames), figsize=(len(layernames)*4, 5.5))
+for li, layername in enumerate(layernames):
+    df = meta_info_df[(meta_info_df.netname==netname) & (meta_info_df.layername==layername)
+                      & (meta_info_df.RFrsz==False)]
+    plt.sca(axs[li])
+    _shaded_errorbar_arr(normscore_mean1_col[df.index].reshape(-1, 100), color="b", alpha=0.3, errtype="std",
+                         label="DeePSim", )
+    _shaded_errorbar_arr(normscore_mean2_col[df.index].reshape(-1, 100), color="r", alpha=0.3, errtype="std",
+                         label="BG CholCMA")
+    _shaded_errorbar_arr(normscore_mean3_col[df.index].reshape(-1, 100), color="g", alpha=0.3, errtype="std",
+                         label="BG HessCMA")
+    plt.title(f"{netname} {layername} {'RF resize' if RFrsz else ''}")
+    plt.ylabel("Normalized Activation")
+    plt.xlabel("Blocks")
+    if li==len(layernames)-1:
+        plt.legend()  # ["DeePSim", "BG CholCMA", "BG HessCMA"])
+plt.suptitle(f"{netname} {'RF resize' if RFrsz else ''} Average Normalized Optimization trajectory")
+plt.tight_layout()
+saveallforms(figdir, f"{netname}_{'_RFrsz' if RFrsz else ''}_merged_normact_cmp_std", figh)
+figh.show()
+for ax in axs:
+    ax.set_xlim([0, 40])
+    # ax.autoscale(enable=True, axis='y', tight=True)
+    ax.autoscale_view(tight=True, scalex=False, scaley=True)
+saveallforms(figdir, f"{netname}_{'_RFrsz' if RFrsz else ''}_merged_normact_cmp_std_xlim40", figh)
+figh.show()
 
