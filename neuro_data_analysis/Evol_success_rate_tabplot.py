27b1faeaa133bd625dae9a63929e99083740ddac
BinxuWang <binxu.wang@wustl.edu>
2023-05-25 14:10:54 -0500

    Add success rate plot for BigGAN vs FC6 add BigGAN space and BigGAN all separation

--- neuro_data_analysis/Evol_success_rate_tabplot.py
+++ neuro_data_analysis/Evol_success_rate_tabplot.py
@@ -51,6 +51,21 @@ for msk, label in zip([validmsk & V1msk, validmsk & V4msk, validmsk & ITmsk],
     print("end > init BG", sum(msk & (meta_df.p_endinit_1 < 0.01) & (meta_df.t_endinit_1 > 0)))
     print("end < init FC", sum(msk & (meta_df.p_endinit_0 < 0.01) & (meta_df.t_endinit_0 < 0)))
     print("end < init BG", sum(msk & (meta_df.p_endinit_1 < 0.01) & (meta_df.t_endinit_1 < 0)))
+#%%
+""" Separate the success rate of between different BG spaces """
+cls_msk = meta_df.space2 == "BigGAN_class"
+full_msk = meta_df.space2 == "BigGAN"
+for msk, label in zip([validmsk & V1msk, validmsk & V4msk, validmsk & ITmsk],
+                      ["V1", "V4", "IT"]):
+    print(label, f"N={sum(msk)}")
+    Nclass = sum(msk & cls_msk)
+    Nfull = sum(msk & full_msk)
+    print(f"max > init BG class %d / {Nclass}" % sum(msk & cls_msk & (meta_df.p_maxinit_1 < 0.01) & (meta_df.t_maxinit_1 > 0)))
+    print(f"max > init BG full %d / {Nfull}" % sum(msk & full_msk & (meta_df.p_maxinit_1 < 0.01) & (meta_df.t_maxinit_1 > 0)))
+    print(f"end > init BG class %d / {Nclass}" % sum(msk & cls_msk & (meta_df.p_endinit_1 < 0.01) & (meta_df.t_endinit_1 > 0)))
+    print(f"end > init BG full %d / {Nfull}" % sum(msk & full_msk & (meta_df.p_endinit_1 < 0.01) & (meta_df.t_endinit_1 > 0)))
+    print(f"end < init BG class %d / {Nclass}" % sum(msk & cls_msk & (meta_df.p_endinit_1 < 0.01) & (meta_df.t_endinit_1 < 0)))
+    print(f"end < init BG full %d / {Nfull}" % sum(msk & full_msk & (meta_df.p_endinit_1 < 0.01) & (meta_df.t_endinit_1 < 0)))
 
 #%% Use different success criterion and save the result while plotting as curves
 sucs_criterion_label, sucs_label = "max > init, P<0.01", "maxinit"
@@ -105,6 +120,120 @@ saveallforms(outdir, f"evol_{sucs_label}_success_rate_per_area_annot", fig, fmts
 fig.show()
 
 
+
+#%%
+sucs_criterion_label, sucs_label = "max > init, P<0.01", "maxinit"
+FC_suc_msk = (meta_df.p_maxinit_0 < 0.01) & (meta_df.t_maxinit_0 > 0)
+BG_suc_msk = (meta_df.p_maxinit_1 < 0.01) & (meta_df.t_maxinit_1 > 0)
+# sucs_criterion_label, sucs_label = "end > init, P<0.01", "endinit"
+# FC_suc_msk = (meta_df.p_endinit_0 < 0.01) & (meta_df.t_endinit_0 > 0)
+# BG_suc_msk = (meta_df.p_endinit_1 < 0.01) & (meta_df.t_endinit_1 > 0)
+# sucs_criterion_label, sucs_label = "end < init, P<0.01", "endinitdecrs"
+# FC_suc_msk = (meta_df.p_endinit_0 < 0.01) & (meta_df.t_endinit_0 < 0)
+# BG_suc_msk = (meta_df.p_endinit_1 < 0.01) & (meta_df.t_endinit_1 < 0)
+# build a dataframe of success rate for plotting
+SR_df = []
+BG_class_msk = meta_df.space2 == "BigGAN_class"
+BG_all_msk = meta_df.space2 == "BigGAN"
+for msk, label in zip([validmsk & V1msk & BG_class_msk, validmsk & V1msk & BG_all_msk,
+                       validmsk & V4msk & BG_class_msk, validmsk & V4msk & BG_all_msk,
+                       validmsk & ITmsk & BG_class_msk, validmsk & ITmsk & BG_all_msk,],
+                      ["V1 class", "V1 all", "V4 class", "V4 all", "IT class", "IT all",]):
+    SR = edict(label=label, FC_suc=sum(msk & FC_suc_msk), BG_suc=sum(msk & BG_suc_msk), total=sum(msk))
+    SR["FC_rate"] = SR.FC_suc / SR.total
+    SR["BG_rate"] = SR.BG_suc / SR.total
+    SR["FC_CI_1"] = rate_CI(SR.total, SR.FC_suc, 0.05)
+    SR["FC_CI_2"] = rate_CI(SR.total, SR.FC_suc, 0.95)
+    SR["BG_CI_1"] = rate_CI(SR.total, SR.BG_suc, 0.05)
+    SR["BG_CI_2"] = rate_CI(SR.total, SR.BG_suc, 0.95)
+    print(SR)
+    SR_df.append(SR)
+SR_df = pd.DataFrame(SR_df)
+SR_df = SR_df.set_index("label")
+# set dtypes
+SR_df = SR_df.astype({"FC_suc": int, "BG_suc": int, "total": int, "FC_rate": float, "BG_rate": float})
+SR_df.to_csv(join(outdir, f"SuccessRate_space_split_{sucs_label}.csv"))
+
+#%%
+fig, ax = plt.subplots(1, 1, figsize=[3.5, 3.3])
+ax.plot(SR_df.index, SR_df.FC_rate, "o-", label="DeePSim", color="b")#"tab:blue"
+ax.plot(SR_df.index, SR_df.BG_rate, "o-", label="BigGAN", color="r")#"tab:red"
+ax.fill_between(SR_df.index, SR_df.FC_CI_1, SR_df.FC_CI_2, alpha=0.3, color="b")#"tab:blue"
+ax.fill_between(SR_df.index, SR_df.BG_CI_1, SR_df.BG_CI_2, alpha=0.3, color="r")#"tab:red"
+ax.set_ylim([0, 1.05])
+ax.set_ylabel("Success Rate")
+ax.set_xlabel("Visual Area")
+# annotate the number of trials as fraction
+for i, (label, row) in enumerate(SR_df.iterrows()):
+    ax.annotate(f"{int(row.FC_suc)}/{int(row.total)}",
+                xy=(i + 0.02, min(1.0, row.FC_rate+0.04)),
+                ha="center", va="bottom", fontsize=10)
+    ax.annotate(f"{int(row.BG_suc)}/{int(row.total)}",
+                xy=(i + 0.02, max(0.0, row.BG_rate-0.06)),
+                ha="center", va="bottom", fontsize=10)
+
+ax.legend()
+fig.suptitle(f"Success Rate of DeePSim and BigGAN\n{sucs_criterion_label} 90% CI")
+fig.tight_layout()
+saveallforms(outdir, f"evol_{sucs_label}_success_rate_per_space_per_area_annot", fig, fmts=["png", "pdf", "svg"])
+fig.show()
+#%%
+# same plot but with bar and errorbar
+fig, ax = plt.subplots(1, 1, figsize=[4.0, 3.75])
+ax.bar(SR_df.index, SR_df.FC_rate, yerr=[SR_df.FC_rate - SR_df.FC_CI_1, SR_df.FC_CI_2 - SR_df.FC_rate],
+         label="DeePSim", color="b", alpha=0.5, error_kw=dict(ecolor='b', capsize=5))
+# add points
+ax.plot(SR_df.index, SR_df.FC_rate, "o", color="b", alpha=0.7)
+ax.bar(SR_df.index, SR_df.BG_rate, yerr=[SR_df.BG_rate - SR_df.BG_CI_1, SR_df.BG_CI_2 - SR_df.BG_rate],
+            label="BigGAN", color="r", alpha=0.5, error_kw=dict(ecolor='r', capsize=5))
+ax.plot(SR_df.index, SR_df.BG_rate, "o", color="r", alpha=0.7)
+# rename the xticks
+ax.set_xticklabels(["V1\nclass", "V1\nall", "V4\nclass", "V4\nall", "IT\nclass", "IT\nall"])
+ax.set_ylim([0, 1.05])
+ax.set_ylabel("Success Rate")
+ax.set_xlabel("Visual Area")
+ax.set_title(f"Success Rate of DeePSim and BigGAN\n{sucs_criterion_label} 90% CI")
+plt.tight_layout()
+saveallforms(outdir, f"evol_{sucs_label}_success_rate_per_space_per_area_bar", fig, fmts=["png", "pdf", "svg"])
+fig.show()
+
+#%%
+# same plot but with bar and errorbar
+fig, ax = plt.subplots(1, 1, figsize=[4.0, 3.75])
+ax.bar(SR_df.index, SR_df.FC_rate, yerr=[SR_df.FC_rate - SR_df.FC_CI_1, SR_df.FC_CI_2 - SR_df.FC_rate],
+         label="DeePSim", color="b", alpha=0.5, error_kw=dict(ecolor='b', capsize=5))
+# add points
+ax.plot(SR_df.index, SR_df.FC_rate, "o", color="b", alpha=0.7)
+ax.bar(SR_df.index, SR_df.BG_rate, yerr=[SR_df.BG_rate - SR_df.BG_CI_1, SR_df.BG_CI_2 - SR_df.BG_rate],
+            label="BigGAN", color="r", alpha=0.5, error_kw=dict(ecolor='r', capsize=5))
+ax.plot(SR_df.index, SR_df.BG_rate, "o", color="r", alpha=0.7)
+# annotate the number of trials as fraction
+for i, (label, row) in enumerate(SR_df.iterrows()):
+    ax.annotate(f"{int(row.FC_suc)}/{int(row.total)}",
+                xy=(i + 0.02, min(1.0, row.FC_rate+0.05)),
+                ha="center", va="bottom", fontsize=10)
+    ax.annotate(f"{int(row.BG_suc)}/{int(row.total)}",
+                xy=(i + 0.02, max(0.0, row.BG_rate-0.08)),
+                ha="center", va="bottom", fontsize=10)
+# rename the xticks
+ax.set_xticklabels(["V1\nclass", "V1\nall", "V4\nclass", "V4\nall", "IT\nclass", "IT\nall"])
+ax.set_ylim([0, 1.05])
+ax.set_ylabel("Success Rate")
+ax.set_xlabel("Visual Area")
+ax.set_title(f"Success Rate of DeePSim and BigGAN\n{sucs_criterion_label} 90% CI")
+plt.tight_layout()
+saveallforms(outdir, f"evol_{sucs_label}_success_rate_per_space_per_area_bar_annot", fig, fmts=["png", "pdf", "svg"])
+fig.show()
+#%%
+from statsmodels.stats.proportion import proportions_ztest
+# count: number of successes, i.e., A1 and A2
+count = np.array([48, 17])
+# nobs: number of trials, i.e., B1 and B2
+nobs = np.array([83, 23])
+# conduct the two-proportion z-test
+z, p = proportions_ztest(count, nobs)
+# print the z-score and p-value
+print(f'z-score: {z:.2f}, p-value: {p:.4f}')
 #%% Older code
 BFEStats_merge, BFEStats = load_neural_data()
 #%%
