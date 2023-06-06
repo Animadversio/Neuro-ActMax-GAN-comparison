61b3e1e57b6930b50a481539765468d5f023895e
BinxuWang <binxu.wang@wustl.edu>
2023-06-05 21:59:15 -0500

    add plot utils and new stats plots

--- core/utils/stats_utils.py
+++ core/utils/stats_utils.py
@@ -109,6 +109,27 @@ def shaded_errorbar_arr(arr, color="r", alpha=0.3, errtype="sem", ax=None, **kwa
                      label="" if "label" not in kwargs else kwargs["label"]+"_"+errtype)
 
 
+import seaborn as sns
+from scipy.stats import pearsonr, spearmanr
+def scatter_corr(df, x, y, ax=None, corrtype="pearson", **kwargs):
+    if ax is None:
+        ax = plt.gca()
+    # ax.scatter(df[x], df[y], **kwargs)
+    sns.scatterplot(data=df, x=x, y=y, ax=ax, **kwargs)
+    # scipy pearsonr
+    validmsk = np.logical_and(np.isfinite(df[x]), np.isfinite(df[y]))
+    if corrtype.lower() == "pearson":
+        rho, pval = pearsonr(df[x][validmsk], df[y][validmsk], )
+    elif corrtype.lower() == "spearman":
+        rho, pval = spearmanr(df[x][validmsk], df[y][validmsk], )
+    else:
+        raise NotImplementedError
+    ax.set_xlabel(x)
+    ax.set_ylabel(y)
+    ax.set_title(f"{x} vs. {y}\ncorr={rho:.3f} p={pval:.1e} n={validmsk.sum()}")
+    return ax, rho, pval
+
+
 def trivariate_corr(x, y, z):
     """
     x = np.random.normal(0, 1, size=100)
@@ -127,3 +148,31 @@ def trivariate_corr(x, y, z):
     yz_corr = np.corrcoef(y, z)[0, 1]
     r = (xy_corr**2 + xz_corr**2 + yz_corr**2 + 2*xy_corr*xz_corr*yz_corr) / (1 - xy_corr**2 - xz_corr**2- yz_corr**2 + 2*xy_corr*xz_corr*yz_corr)
     return r
+
+
+
+from scipy import stats
+def test_correlation_diff(A, B, C, verbose=False):
+    # Compute the correlations
+    r_AB = np.corrcoef(A, B)[0, 1]
+    r_AC = np.corrcoef(A, C)[0, 1]
+    # Compute the standard error of the difference between the two z scores
+    N = len(A)
+    SE = np.sqrt((2 * (N - 3))**-1)
+    # Apply Fisher's Z transformation
+    z_AB = 0.5 * np.log((1 + r_AB) / (1 - r_AB)) / SE
+    z_AC = 0.5 * np.log((1 + r_AC) / (1 - r_AC)) / SE
+    # Compute the test statistic
+    z = (z_AB - z_AC)
+    # Compute the p-value
+    p_value = 1 - stats.norm.cdf(z)
+    p_value_rev = stats.norm.cdf(z)
+    p_2sided = 2 * np.min([p_value, p_value_rev])
+    if verbose:
+        print(f"r_AB={r_AB:.3f}, r_AC={r_AC:.3f}, z_AB={z_AB:.3f}, z_AC={z_AC:.3f}"
+              f"\nz={z:.3f}, p={p_2sided:.1e} (2 side), p={min(p_value, p_value_rev):.1e} (1 side) N={N:d}")
+    return z, p_2sided
+
+
+def test_correlation_diff_df(df, colA, colB, colC, verbose=False):
+    return test_correlation_diff(df[colA], df[colB], df[colC], verbose=verbose)
