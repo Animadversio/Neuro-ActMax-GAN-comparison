de749f44287610a12472d24956a6d25806c13a89
BinxuWang <binxu.wang@wustl.edu>
2023-12-21 23:16:11 -0600

    add temporal corr R2

--- neuro_data_analysis/Evol_cross_pred_pipeline.py
+++ neuro_data_analysis/Evol_cross_pred_pipeline.py
@@ -14,7 +14,14 @@ from os.path import join
 # from core.utils.CNN_scorers import TorchScorer, load_featnet
 os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 #%%
-macstim_rootdir = "/Users/binxuwang/Network_mapping"
+import platform
+if platform.system() == "Windows":
+    stim_rootdir = "S:"
+    device = "cuda"
+elif platform.system() == "Darwin":
+    stim_rootdir = "/Users/binxuwang/Network_mapping"
+    device = "mps"
+
 # load the neural data
 BFEStats_merge, BFEStats = load_neural_data()
 #%%
@@ -25,12 +32,13 @@ rsp_wdws = [range(50, 200), range(0, 50), range(50, 100), range(100, 150), range
 # image paths: (n_images, )
 # response: (n_images, n_time)
 Expi = 155
+Expi = 165
 imgfps0, resp_mat0, gen_vec0 = load_img_resp_pairs_multiwindow(BFEStats, Expi,
-                         "Evol", thread=0, stimdrive=macstim_rootdir,
-                         output_fmt="vec", rsp_wdws=rsp_wdws)
+                         "Evol", thread=0, stimdrive=stim_rootdir,
+                                                               output_fmt="vec", rsp_wdws=rsp_wdws)
 imgfps1, resp_mat1, gen_vec1 = load_img_resp_pairs_multiwindow(BFEStats, Expi,
-                         "Evol", thread=1, stimdrive=macstim_rootdir,
-                         output_fmt="vec", rsp_wdws=rsp_wdws)
+                         "Evol", thread=1, stimdrive=stim_rootdir,
+                                                               output_fmt="vec", rsp_wdws=rsp_wdws)
 #%%
 # use default transform for the image, including RGB norm and resize
 evol_ds0 = ImagePathDataset(imgfps0, resp_mat0, transform=None, img_dim=(224, 224))
@@ -61,8 +69,8 @@ def extract_feat(dataset, featnet, device="mps", batch_size=10):
     return feattsr
 # extract features from image sequence using some encoder / PC space
 # features: (n_images, n_features)
-feattsr0 = extract_feat(evol_ds0, featnet, device="mps", batch_size=15)
-feattsr1 = extract_feat(evol_ds1, featnet, device="mps", batch_size=15)
+feattsr0 = extract_feat(evol_ds0, featnet, device=device, batch_size=15)
+feattsr1 = extract_feat(evol_ds1, featnet, device=device, batch_size=15)
 #%%
 # loop over all the window pairs
 # build prediction / regression model for window 1; test on window 2
@@ -70,22 +78,54 @@ from sklearn.linear_model import RidgeCV, LassoCV, MultiTaskLassoCV
 from sklearn.model_selection import cross_val_score
 # split the data into train and test
 from sklearn.model_selection import train_test_split
+def R2score_per_bin(model, feattsr, resp_mat):
+    predmat = model.predict(feattsr)
+    residue_ss = np.mean((predmat - resp_mat) ** 2, axis=0)
+    total_ss = np.mean((resp_mat.mean(axis=0) - resp_mat) ** 2, axis=0)
+    R2_vec = 1 - residue_ss / total_ss
+    return R2_vec
+
+
+def corrscore_per_bin(model, feattsr, resp_mat):
+    predmat = model.predict(feattsr)
+    corr_vec = np.corrcoef(predmat.T, resp_mat.T)[np.arange(predmat.shape[1]),
+                np.arange(predmat.shape[1], predmat.shape[1] + resp_mat.shape[1])]
+    return corr_vec
 #%%
 (feattsr0_train, feattsr0_test,
- resp_mat0_train, resp_mat0_test) = train_test_split(
-    feattsr0, resp_mat0,
-    test_size=0.2, random_state=42, shuffle=True,)
+ resp_mat0_train, resp_mat0_test,
+ gen_vec0_train, gen_vec0_test) = train_test_split(
+    feattsr0, resp_mat0, gen_vec0,
+    test_size=0.2, random_state=41, shuffle=True,)
 (feattsr1_train, feattsr1_test,
-    resp_mat1_train, resp_mat1_test) = train_test_split(
-        feattsr1, resp_mat1,
-    test_size=0.2, random_state=42, shuffle=True,)
+    resp_mat1_train, resp_mat1_test,
+    gen_vec1_train, gen_vec1_test) = train_test_split(
+        feattsr1, resp_mat1, gen_vec1,
+    test_size=0.2, random_state=41, shuffle=True,)
 #%%
 regmodel0 = RidgeCV(alphas=np.logspace(-3, 3, 10), )
 regmodel0.fit(feattsr0_train, resp_mat0_train)
-print(regmodel0.score(feattsr0_train, resp_mat0_train))
-print(regmodel0.score(feattsr0_test, resp_mat0_test))
-print(regmodel0.score(feattsr1_train, resp_mat1_train))
-print(regmodel0.score(feattsr1_test, resp_mat1_test))
+print("DeePSim train (R2)", R2score_per_bin(regmodel0, feattsr0_train, resp_mat0_train))
+print("DeePSim test (R2)", R2score_per_bin(regmodel0, feattsr0_test, resp_mat0_test))
+print("BigGAN train (R2)", R2score_per_bin(regmodel0, feattsr1_train, resp_mat1_train))
+print("BigGAN test (R2)", R2score_per_bin(regmodel0, feattsr1_test, resp_mat1_test))
+print("DeePSim train (corr)", corrscore_per_bin(regmodel0, feattsr0_train, resp_mat0_train))
+print("DeePSim test (corr)", corrscore_per_bin(regmodel0, feattsr0_test, resp_mat0_test))
+print("BigGAN train (corr)", corrscore_per_bin(regmodel0, feattsr1_train, resp_mat1_train))
+print("BigGAN test (corr)", corrscore_per_bin(regmodel0, feattsr1_test, resp_mat1_test))
+
+#%%
+regmodel0 = MultiTaskLassoCV(alphas=np.logspace(-3, 3, 10), )
+regmodel0.fit(feattsr0_train, resp_mat0_train)
+print("DeePSim train (R2)", R2score_per_bin(regmodel0, feattsr0_train, resp_mat0_train))
+print("DeePSim test (R2)", R2score_per_bin(regmodel0, feattsr0_test, resp_mat0_test))
+print("BigGAN train (R2)", R2score_per_bin(regmodel0, feattsr1_train, resp_mat1_train))
+print("BigGAN test (R2)", R2score_per_bin(regmodel0, feattsr1_test, resp_mat1_test))
+#%%
+print("DeePSim train (corr)", corrscore_per_bin(regmodel0, feattsr0_train, resp_mat0_train))
+print("DeePSim test (corr)", corrscore_per_bin(regmodel0, feattsr0_test, resp_mat0_test))
+print("BigGAN train (corr)", corrscore_per_bin(regmodel0, feattsr1_train, resp_mat1_train))
+print("BigGAN test (corr)", corrscore_per_bin(regmodel0, feattsr1_test, resp_mat1_test))
 
 #%%
 regmodel1 = RidgeCV(alphas=np.logspace(-3, 3, 10), )
@@ -95,6 +135,20 @@ print(regmodel1.score(feattsr0_test, resp_mat0_test))
 print(regmodel1.score(feattsr1_train, resp_mat1_train))
 print(regmodel1.score(feattsr1_test, resp_mat1_test))
 #%%
+regmodel1 = MultiTaskLassoCV(alphas=np.logspace(-3, 3, 10), )
+regmodel1.fit(feattsr1_train, resp_mat1_train)
+print("DeePSim train (R2)", R2score_per_bin(regmodel1, feattsr0_train, resp_mat0_train))
+print("DeePSim test (R2)", R2score_per_bin(regmodel1, feattsr0_test, resp_mat0_test))
+print("BigGAN train (R2)", R2score_per_bin(regmodel1, feattsr1_train, resp_mat1_train))
+print("BigGAN test (R2)", R2score_per_bin(regmodel1, feattsr1_test, resp_mat1_test))
+#%%
+print("DeePSim train (corr)", corrscore_per_bin(regmodel1, feattsr0_train, resp_mat0_train))
+print("DeePSim test (corr)", corrscore_per_bin(regmodel1, feattsr0_test, resp_mat0_test))
+print("BigGAN train (corr)", corrscore_per_bin(regmodel1, feattsr1_train, resp_mat1_train))
+print("BigGAN test (corr)", corrscore_per_bin(regmodel1, feattsr1_test, resp_mat1_test))
+
+
+#%%
 regmodel01 = RidgeCV(alphas=np.logspace(-3, 3, 10), )
 regmodel01.fit(torch.cat([feattsr0_train, feattsr1_train], dim=0),
                 np.concatenate([resp_mat0_train, resp_mat1_train], axis=0))
@@ -103,14 +157,47 @@ print(regmodel01.score(feattsr0_test, resp_mat0_test))
 print(regmodel01.score(feattsr1_train, resp_mat1_train))
 print(regmodel01.score(feattsr1_test, resp_mat1_test))
 #%%
+regmodel01 = MultiTaskLassoCV(alphas=np.logspace(-3, 3, 10), )
+regmodel01.fit(torch.cat([feattsr0_train, feattsr1_train], dim=0),
+                np.concatenate([resp_mat0_train, resp_mat1_train], axis=0))
+print(regmodel01.score(feattsr0_train, resp_mat0_train))
+print(regmodel01.score(feattsr0_test, resp_mat0_test))
+print(regmodel01.score(feattsr1_train, resp_mat1_train))
+print(regmodel01.score(feattsr1_test, resp_mat1_test))
+#%%
+regmodel01.predict(feattsr0_test)
+#%%
+resp_mat0_test
+#%%
+#visualize the response mat
+gen_sortidx = gen_vec0_test.argsort()
+plt.figure(figsize=[5, 10.4])
+plt.subplot(121)
+plt.imshow(regmodel01.predict(feattsr0_test)[gen_sortidx,], aspect="auto")
+plt.subplot(122)
+plt.imshow(resp_mat0_test[gen_sortidx,], aspect="auto")
+plt.show()
+#%%
+#visualize the response mat
+gen_sortidx = gen_vec0_train.argsort()
+plt.figure(figsize=[5, 10.4])
+plt.subplot(121)
+plt.imshow(regmodel01.predict(feattsr0_train)[gen_sortidx,], aspect="auto")
+plt.subplot(122)
+plt.imshow(resp_mat0_train[gen_sortidx,], aspect="auto")
+plt.show()
+#%%
+
+
+print(R2score_per_bin(regmodel01, feattsr0_train, resp_mat0_train))
+print(R2score_per_bin(regmodel01, feattsr0_test, resp_mat0_test))
+print(R2score_per_bin(regmodel01, feattsr1_train, resp_mat1_train))
+print(R2score_per_bin(regmodel01, feattsr1_test, resp_mat1_test))
+
+#%%
 regmodel0.predict(feattsr0_test).shape
 #%%
-regmodel0 = MultiTaskLassoCV(alphas=np.logspace(-3, 3, 10), )
-regmodel0.fit(feattsr0_train, resp_mat0_train)
-print(regmodel0.score(feattsr0_train, resp_mat0_train))
-print(regmodel0.score(feattsr0_test, resp_mat0_test))
-print(regmodel0.score(feattsr1_train, resp_mat1_train))
-print(regmodel0.score(feattsr1_test, resp_mat1_test))
+np.corrcoef(regmodel01.coef_)
 #%%
 # get prediction tensor (n_images, n_time, n_time)
 
